c06e90018970a1773b6ab3432d2c101e3301e4d0
Kyle <kyle123088@gmail.com>
2025-03-08 16:21:39 +0800

    商品功能 - 實作查詢商品列表(分頁)

--- src/main/java/com/kyle/springbootmall/controller/ProductController.java
+++ src/main/java/com/kyle/springbootmall/controller/ProductController.java
@@ -6,21 +6,23 @@ import com.kyle.springbootmall.dto.ProductRequest;
 import com.kyle.springbootmall.model.Product;
 import com.kyle.springbootmall.service.ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    // @RequestParam ： 從url的請求參數
-    // ProductCategory category ： 前端可以透過傳進來的 category 的值，知道要查看哪一類商品
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
             // 查詢條件 Filtering
@@ -29,13 +31,19 @@ public class ProductController {
 
             // 排序 Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+
+            // 分頁 Pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
     ) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
