abcda968103886c0d289cf3968641cb2d8898474
Kyle <kyle123088@gmail.com>
2025-03-08 14:49:11 +0800

    商品功能 - 實作查詢商品列表(排序)

--- src/main/java/com/kyle/springbootmall/controller/ProductController.java
+++ src/main/java/com/kyle/springbootmall/controller/ProductController.java
@@ -23,12 +23,19 @@ public class ProductController {
     // ProductCategory category ： 前端可以透過傳進來的 category 的值，知道要查看哪一類商品
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
+            // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
-            @RequestParam(required = false) String search
+            @RequestParam(required = false) String search,
+
+            // 排序 Sorting
+            @RequestParam(defaultValue = "created_date") String orderBy,
+            @RequestParam(defaultValue = "desc") String sort
     ) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
+        productQueryParams.setOrderBy(orderBy);
+        productQueryParams.setSort(sort);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
