80a22659d02b48ae31833f673ab5a2e4cab1ba27
Kyle <kyle123088@gmail.com>
2025-03-08 17:10:23 +0800

    商品功能 - 實作查詢商品列表(分頁 + 總筆數)

--- src/main/java/com/kyle/springbootmall/controller/ProductController.java
+++ src/main/java/com/kyle/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.kyle.springbootmall.dto.ProductQueryParams;
 import com.kyle.springbootmall.dto.ProductRequest;
 import com.kyle.springbootmall.model.Product;
 import com.kyle.springbootmall.service.ProductService;
+import com.kyle.springbootmall.util.Page;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -24,7 +25,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -45,9 +46,21 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        // 取得 product list
         List<Product> productList = productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        // 取得 product 總數
+        // 需要帶 productQueryParams 參數是因為不同條件下，會篩選出不同筆結果
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
     @GetMapping("/products/{productId}")
